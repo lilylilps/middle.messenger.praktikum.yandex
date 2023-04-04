519f5935ac777c5b17dacfd33b78df8133db5e88
Lily <lilya.makarova.2020@gmail.com>
2023-04-04 22:15:28 +0300

    upd readme and server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const fallback = require("express-history-api-fallback");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const path = require('path');
-
-app.use('/', express.static(path.join(__dirname, 'dist')));
+const root = './dist/';
+app.use(express.static(root));
+app.use(fallback('index.html', {root}));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
